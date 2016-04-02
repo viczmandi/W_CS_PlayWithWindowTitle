4fc2a0804a46bd615da37ad6e2d6f5ded0bae245
Viczmándi Gergely <viczmandi@gmail.com>
2016-04-02 22:38:54 +0200

    First commit/push

---
+++ PlayWithWindowTitle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayWithWindowTitle
+{
+    class Program
+    {
+        const int maxProgressBarLength = 28;
+        const string progressBarElement = "-";
+        const string codecool = "CODECOOL";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Look at the title! How awesome is that?!");
+
+            var title = "";
+
+            do
+            {
+                title = GenerateTitle(title);
+                Console.Title = title;
+                Thread.Sleep(100);
+            } while (true);
+        }
+
+        private static string GenerateTitle(string currentTitle)
+        {
+            int textStart = 11;
+            int textEnd = 18;
+
+            if (currentTitle.Length < textStart || currentTitle.Length > textEnd)
+            {
+                currentTitle += progressBarElement;
+            }
+            else
+            {
+                currentTitle += codecool[currentTitle.Length - textStart];
+            }
+
+            if (currentTitle.Length > maxProgressBarLength)
+            {
+                currentTitle = progressBarElement;
+            }
+
+            return currentTitle;
+        }
+    }
+
+}
